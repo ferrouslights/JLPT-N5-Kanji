f99bf596c5feca935c2d71e163a5094cb02b4c91
Benjamin Bridges <benjaminbridges.wayne@gmail.com>
2021-09-10 15:03:14 -0500

    Added some characters for testing
    75 to go after that :')

---
+++ src/components/Kanji.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+const Kanji = () => {
+    const kanjiList = [
+    {id: 0,
+    character: '日',
+    onReading: 'にち、じつ',
+    kunReading: 'ひ、び、か',
+    meaning: 'Day, Sun',
+    },  
+    {id: 1,
+    character: '一',
+    onReading: 'いち',
+    kunReading: 'ひと',
+    meaning: 'One',
+    },
+    {id: 2,
+    character: '国',
+    onReading: 'こく',
+    kunReading: 'くに',
+    meaning: 'Country',
+    },
+    {id: 3,
+    character: '人',
+    onReading: 'じん、にん',
+    kunReading: 'ひと',
+    meaning: 'Person',
+    },
+    {id: 4,
+    character: '年',
+    onReading: 'ねん',
+    kunReading: 'とし',
+    meaning: 'Year (counter)',
+    },
+    
+    ]
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Kanji;
